063435c9397def417f6810b5b6f51bdd260da69c
satyaejjurotu <satyaejjurotu@yahoo.com>
2022-02-14 21:18:59 +0400

    First commit

--- SatyanarayanaE-Lab-DBMS AssignmentSolution.sql
+++ SatyanarayanaE-Lab-DBMS AssignmentSolution.sql
@@ -1,4 +1,8 @@
 Create Database if not exists `orderdirectory` ;
+
+-- 1. You are required to create tables for supplier,customer,category,product,productDetails,order,
+-- rating to store the data for the E-commerce with the schema definition given below.
+
 use `orderdirectory`;
 create table if not exists `supplier`(
 `SUPP_ID` int primary key,
@@ -27,6 +31,8 @@ CREATE TABLE IF NOT EXISTS `order` (   `ORD_ID` INT NOT NULL,   `ORD_AMOUNT` INT
 CREATE TABLE IF NOT EXISTS `rating` (   `RAT_ID` INT NOT NULL,   `CUS_ID` INT NOT NULL,   `SUPP_ID` INT NOT NULL,   `RAT_RATSTARS` INT NOT NULL,   PRIMARY KEY (`RAT_ID`),   FOREIGN KEY (`SUPP_ID`) REFERENCES SUPPLIER (`SUPP_ID`),   FOREIGN KEY (`CUS_ID`) REFERENCES CUSTOMER(`CUS_ID`)
   );
 
+-- 2. Insert the following data in the table created above
+
 insert into `supplier` values(1,"Rajesh Retails","Delhi",'1234567890'); insert into `supplier` values(2,"Appario Ltd.","Mumbai",'2589631470'); insert into `supplier` values(3,"Knome products","Banglore",'9785462315'); insert into `supplier` values(4,"Bansal Retails","Kochi",'8975463285');
 insert into `supplier` values(5,"Mittal Ltd.","Lucknow",'7898456532');
 INSERT INTO `CUSTOMER` VALUES(1,"AAKASH",'9999999999',"DELHI",'M'); INSERT INTO `CUSTOMER` VALUES(2,"AMAN",'9785463215',"NOIDA",'M'); INSERT INTO `CUSTOMER` VALUES(3,"NEHA",'9999999999',"MUMBAI",'F'); INSERT INTO `CUSTOMER` VALUES(4,"MEGHA",'9994562399',"KOLKATA",'F');
@@ -50,26 +56,38 @@ INSERT INTO `RATING` VALUES(3,5,1,5);
 INSERT INTO `RATING` VALUES(4,1,3,2);
 INSERT INTO `RATING` VALUES(5,4,5,4);
 
--- 3. Display the number of the customer group by their genders who have placed any order of amount
--- greater than or equal to Rs. 3000.
+-- 3. Display the number of the customer group by their genders who have placed any order of amount greater than or equal to Rs. 3000.
 
 select customer.cus_gender,count(customer.cus_gender) as count from customer inner join `order` on customer.cus_id=`order`.cus_id where `order`.ord_amount>=3000 group by 
 customer.cus_gender;
 
+-- 4. Display all the orders along with the product name ordered by a customer having Customer_Id=2.
 
 select `order`.*,product.pro_name from `order`,product_details,product where `order`.cus_id=2 and `order`.prod_id=product_details.prod_id and 
 product_details.prod_id=product.pro_id;
 
+-- 5. Display the Supplier details who can supply more than one product.
+
 select supplier.*  from supplier, product_details where supplier. supp_id in (select product_details.
 supp_id from product_details group by product_details. supp_id having count(product_details. supp_id)>1) group by supplier. supp_id;
 
+-- 6. Find the category of the product whose order amount is minimum.
+
 select category.* from `order` inner join product_details on `order`.prod_id=product_details.
-prod_id inner join product on product.pro_id=product_details.pro_id inner join category on category.cat_id=product.cat_id having min(`order`.ord_amount);
+prod_id inner join product on product.pro_id=product_details.pro_id inner join category on category.cat_id=product.cat_id 
+having min(`order`.ord_amount);
+
+-- 7.	Display the Id and Name of the Product ordered after “2021-10-05”.
 
 select product.pro_id,product.pro_name from `order` inner join product_details on product_details.
 prod_id=`order`.prod_id inner join product on product.pro_id=product_details.pro_id where `order`.ord_date>"2021-10-05";
 
+-- 8.	Display customer name and gender whose names start or end with character 'A'.
+
 select customer.cus_name  ,customer.cus_gender from customer where customer.cus_name like 'A%' or Customer.cus_name like '%A';
+
+-- 9.	Create a stored procedure to display the Rating for a Supplier if any along with the Verdict on
+-- that rating if any like if rating >4 then “Genuine Supplier” if rating >2 “Average Supplier” else “Supplier should not be considered”.
 
 select supplier.supp_id, supplier.supp_name,rating.rat_ratstars,
 CASE
@@ -79,5 +97,3 @@ ELSE 'Supplier should not be considered'
 END AS verdict from rating inner join supplier on supplier.supp_id=rating. supp_id;
 
 call `proc`();
-
-show create procedure `proc-1`;
